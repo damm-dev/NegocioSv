525b3f414b2254608900067c9b3e71992bac3347
Gerson Juárez <111896881+JuarezGer@users.noreply.github.com>
2025-11-21 17:37:57 -0600

    Configuración Inicial

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Example() {
+    return (
+        <div style={{ padding: '20px', border: '1px solid blue' }}>
+            <h1>¡Hola desde React en Laravel! ⚛️</h1>
+            <p>Este componente se renderiza a través de Vite.</p>
+        </div>
+    );
+}
+
+export default Example;

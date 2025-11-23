1786535c77495b0ed5a3506c84f0eabcf30198c4
Jda <114841566+JCruz16@users.noreply.github.com>
2025-11-23 14:49:17 -0600

    mostrar lista de negocios y negocio por id

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NegocioController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
@@ -7,7 +9,15 @@ Route::get('/ping', function () {
         'message' => 'API LARAVEL FUNCIONANDO 🚀'
     ]);
 });
-
+//Rutas de autenticación
 Route::post('/auth/registro_usuario', [AuthController::class, 'register']);
 Route::post('/auth/registro_negocios', [AuthController::class, 'registerBusiness']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+
+//Rutas de la información de usuario
+//Route::get('/user/userInfomation/{id}', [UsuarioController::class, 'userInformation']);
+
+//esta ruta obtiene la lista de negocios
+Route::get('/listas_negocios', [NegocioController::class, 'listaNegocios']);
+//esta ruta obtiene los datos de un negocio en específico
+Route::get('/negocios/{id}', [NegocioController::class, 'negocioPorId']);
